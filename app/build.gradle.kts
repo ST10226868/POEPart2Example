aa676bd3332390b4e05100be134c913a151e5ec1
St10226868 <St10226868@vcconnect.edu.za>
2024-06-08 20:35:43 +0200

    picture can be added on the database in TimesheetActivity.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+
 }
 
 android {
@@ -50,6 +51,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+
     implementation("com.google.android.material:material:1.1.0")
 
 }
+
--- app/src/main/java/com/example/poepart2example/ViewEntriesActivity.kt
+++ app/src/main/java/com/example/poepart2example/ViewEntriesActivity.kt
@@ -66,4 +66,3 @@ class ViewEntriesActivity : AppCompatActivity() {
             }
     }
 }
-
